3afbf48a2afb1d39e122916fac90ba25a265d549
megellis <mpellis@dons.usfca.edu>
2015-07-14 10:22:56 -0700

    test

--- while loops.py
+++ while loops.py
@@ -19,22 +19,44 @@ while i < 10:
 
 print s
 
-s = ""
-while True:
-    input = raw_input("enter line: ")
-    if input == ".":
-        break
-    s += input
-print s
+#s = ""
+#while True:
+ #   input = raw_input("enter line: ")
+  #  if input == ".":
+   #     break
+    #s += input
+#print s
 
-v = 0
-while True:
-    input = raw_input("enter line: ")
-    if input == ".":
-        break
-    if not isdigit(input):
-        print "not an int"
-        continue
-    v += int(input)
+#v = 0
+#while True:
+    #input = raw_input("enter line: ")
+    #if input == ".":
+     #   break
+    #if not isdigit(input):
+     #   print "not an int"
+      #  continue
+    #v += int(input)
 
-print v
+#print v
+
+# nested loops
+
+print "Times Table"
+n = 0
+m = 0
+stop = 5
+while n <= 5:
+    print "%d %d %d" %(n, m, n*m)
+    n += 1
+    m +=1
+
+print "\n"
+print "Times Table"
+n = 0
+stop = 4
+while n <= stop:
+    m = 0
+    while m <= stop:
+        print "%d %d %d" %(n, m, n*m)
+        m += 1
+    n +=1
